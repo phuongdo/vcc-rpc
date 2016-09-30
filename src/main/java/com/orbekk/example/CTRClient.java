6652dbd6660803be81280e701be8b17335fc69a8
pdoviet <phuongdoviet@admicro.vn>
2016-09-30 16:48:41 +0700

    enhance performance

--- src/main/java/com/orbekk/example/CTRClient.java
+++ src/main/java/com/orbekk/example/CTRClient.java
@@ -1,6 +1,13 @@
 package com.orbekk.example;
 
+import com.google.protobuf.BlockingRpcChannel;
 import com.google.protobuf.RpcCallback;
+import com.google.protobuf.RpcController;
+import com.google.protobuf.ServiceException;
+import com.googlecode.protobuf.socketrpc.RpcChannels;
+import com.googlecode.protobuf.socketrpc.RpcConnectionFactory;
+import com.googlecode.protobuf.socketrpc.SocketRpcConnectionFactories;
+import com.googlecode.protobuf.socketrpc.SocketRpcController;
 import com.orbekk.example.CTR;
 import com.orbekk.protobuf.Rpc;
 import com.orbekk.protobuf.RpcChannel;
@@ -13,62 +20,88 @@ import java.util.concurrent.CountDownLatch;
 public class CTRClient {
 
     public void runClient(int port) {
-        RpcChannel channel = null;
+//        RpcChannel channel = null;
+        BlockingRpcChannel channel = null;
         try {
-            channel = RpcChannel.createOrNull("localhost", port);
-            CTR.CTRPredictionService service =
-                    CTR.CTRPredictionService.newStub(channel);
-            printData(service);
+            // Create channel
+            RpcConnectionFactory connectionFactory = SocketRpcConnectionFactories
+                    .createRpcConnectionFactory("192.168.23.52", 9999);
+//            channel = RpcChannel.createOrNull("192.168.23.52", port);
+            channel = RpcChannels.newBlockingRpcChannel(connectionFactory);
+            // Call service
+            CTR.CTRPredictionService.BlockingInterface service = CTR.CTRPredictionService.newBlockingStub(channel);
+//            CTR.CTRPredictionService service =
+//                    CTR.CTRPredictionService.newStub(channel);
+
+            RpcController controller = new SocketRpcController();
+            printData(controller, service);
         } finally {
             if (channel != null) {
-                channel.close();
+//                channel.close();
             }
         }
 
     }
 
-    public void printData(CTR.CTRPredictionService service) {
-        Rpc rpc = new Rpc();  // Represents a single rpc call.
-
-        int weekday = 1;
-        int hour = 2;
-        int bannerId = 3;
-        int geographic = 4;
-        int zoneId = 5;
-        int guid = 6;
-        String domain = "xxx";
-        int osCode = 8;
-        int browserCode = 9;
+    public void printData(RpcController controller, CTR.CTRPredictionService.BlockingInterface service) {
+//        Rpc rpc = new Rpc();  // Represents a single rpc call.
 
 
-        CTR.Request request = CTR.Request.newBuilder()
-                .setWeekday(weekday)
-                .setHour(hour)
-                .setBannerId(bannerId)
-                .setGeographic(geographic)
-                .setZoneId(zoneId)
-                .setGuid(guid)
-                .setDomain(domain)
-                .setOsCode(osCode)
-                .setBrowserCode(browserCode)
-                .build();
+        for (int i = 0; i < 1000; i++) {
+            int weekday = 1;
+            int hour = 2;
+            int bannerId = 3;
+            int geographic = 4;
+            int zoneId = 5;
+            int guid = 6;
+            String domain = "xxx";
+            int osCode = 8;
+            int browserCode = 9;
 
 
-        // An RPC call is asynchronous. A CountDownLatch is a nice way to wait
-        // for the callback to finish.
-        final CountDownLatch doneSignal = new CountDownLatch(1);
-        service.getPrediction(rpc, request, new RpcCallback<CTR.Response>() {
-            @Override
-            public void run(CTR.Response reply) {
-                System.out.println(reply.getBannerId() + ":" + reply.getCtrPrediction());
-                doneSignal.countDown();
+            CTR.Request request = CTR.Request.newBuilder()
+                    .setWeekday(weekday)
+                    .setHour(hour)
+                    .setBannerId(bannerId)
+                    .setGeographic(geographic)
+                    .setZoneId(zoneId)
+                    .setGuid(guid)
+                    .setDomain(domain)
+                    .setOsCode(osCode)
+                    .setBrowserCode(browserCode)
+                    .build();
+
+
+            // An RPC call is asynchronous. A CountDownLatch is a nice way to wait
+            // for the callback to finish.
+//            final CountDownLatch doneSignal = new CountDownLatch(1);
+
+            try {
+                CTR.Response myResponse = service.getPrediction(controller, request);
+                System.out.println(myResponse.getCtrPrediction());
+            } catch (ServiceException e) {
+                e.printStackTrace();
             }
-        });
 
-        try {
-            doneSignal.await();
-        } catch (InterruptedException e) {
-            System.out.println("Interrupted while waiting for fortune. :(");
+//            service.getPrediction(rpc, request, new RpcCallback<CTR.Response>() {
+//                @Override
+//                public void run(CTR.Response reply) {
+//                    System.out.println(reply.getBannerId() + ":" + reply.getCtrPrediction());
+////                    doneSignal.countDown();
+//                }
+//            });
+//
+//            try {
+//                rpc.await();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+
+//            try {
+//                doneSignal.await();
+//            } catch (InterruptedException e) {
+//                System.out.println("Interrupted while waiting for fortune. :(");
+//            }
         }
     }
 
@@ -78,6 +111,9 @@ public class CTRClient {
 //            System.exit(1);
 //        }
 //        new CTRClient().runClient(Integer.valueOf(args[0]));
+
+        long startTime = System.currentTimeMillis();
         new CTRClient().runClient(9999);
+        System.out.println("Time: " + (System.currentTimeMillis() - startTime));
     }
 }
--- src/main/java/com/orbekk/example/CTRServer.java
+++ src/main/java/com/orbekk/example/CTRServer.java
@@ -2,10 +2,15 @@ package com.orbekk.example;
 
 import com.google.protobuf.RpcCallback;
 import com.google.protobuf.RpcController;
+import com.google.protobuf.ServiceException;
+import com.googlecode.protobuf.socketrpc.RpcServer;
+import com.googlecode.protobuf.socketrpc.ServerRpcConnectionFactory;
+import com.googlecode.protobuf.socketrpc.SocketRpcConnectionFactories;
 import com.orbekk.example.CTR;
 import com.orbekk.protobuf.SimpleProtobufServer;
 
 import java.util.Random;
+import java.util.concurrent.Executors;
 
 /**
  * Created by pdoviet on 9/30/16.
@@ -13,13 +18,32 @@ import java.util.Random;
 public class CTRServer {
 
 
-    public class CTRPredictionService extends CTR.CTRPredictionService {
+    public class MyBlockingInterfaceImpl implements CTR.CTRPredictionService.BlockingInterface {
         Random random = new Random();
+//
+//        @Override
+//        public void getPrediction(RpcController controller, CTR.Request request,
+//                                  RpcCallback<CTR.Response> done) {
+////            String fortune = fortunes[random.nextInt(fortunes.length)];
+//
+//            long sum = 0;
+//            for (int i = 1; i < 100000; i++) {
+//                for (int j = 1; j < 100000; j++) {
+//                    sum = i + j;
+//                }
+//            }
+//
+//
+//            float pred = (float) random.nextInt(10000) / sum;
+//            int bannerid = request.getBannerId();
+//            CTR.Response.Builder reply =
+//                    CTR.Response.newBuilder().
+//                            setBannerId(bannerid).setCtrPrediction(pred);
+//            done.run(reply.build());
+//        }
 
         @Override
-        public void getPrediction(RpcController controller, CTR.Request request,
-                                  RpcCallback<CTR.Response> done) {
-//            String fortune = fortunes[random.nextInt(fortunes.length)];
+        public CTR.Response getPrediction(RpcController controller, CTR.Request request) throws ServiceException {
 
             long sum = 0;
             for (int i = 1; i < 100000; i++) {
@@ -34,19 +58,32 @@ public class CTRServer {
             CTR.Response.Builder reply =
                     CTR.Response.newBuilder().
                             setBannerId(bannerid).setCtrPrediction(pred);
-            done.run(reply.build());
+
+            return reply.build();
         }
     }
 
     public void runServer(int port) {
-        SimpleProtobufServer server = SimpleProtobufServer.create(port);
-        server.registerService(new CTRPredictionService());
-        server.start();
-        System.out.println("Running server on port " + server.getPort());
-        try {
-            server.join();
-        } catch (InterruptedException e) {
-        }
+//        SimpleProtobufServer server = SimpleProtobufServer.create(port);
+//        server.registerService(new CTRPredictionService());
+//        server.start();
+//        System.out.println("Running server on port " + server.getPort());
+//        try {
+//            server.join();
+//        } catch (InterruptedException e) {
+//        }
+
+        int threadPoolSize = 20;
+        // Start server
+        ServerRpcConnectionFactory rpcConnectionFactory = SocketRpcConnectionFactories
+                .createServerRpcConnectionFactory(9999);
+        RpcServer server = new RpcServer(rpcConnectionFactory,
+                Executors.newFixedThreadPool(threadPoolSize), true);
+//        server.registerService(new CTRPredictionService()); // For non-blocking impl
+        server.registerBlockingService(CTR.CTRPredictionService
+                .newReflectiveBlockingService(new MyBlockingInterfaceImpl())); // For blocking impl
+        server.run();
+
     }
 
     public static void main(String[] args) {
